64ea181be9b3871fc236a9237b0240fcd6745d98
Rafaela Duo <rafaelagitti@gmail.com>
2026-04-05 14:39:34 -0300

    Add models e troca da tela inicial

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,17 +1,16 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
-namespace MauiAppMinhasCompras
+﻿namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            //MainPage = new AppShell();
+
+            MainPage = new NavigationPage( new Views.ListaProduto());
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+       
     }
 }
