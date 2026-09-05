3045594576557d9fc807243cbc473d190bc97271
rafaeladuo <rafaelagitti@gmail.com>
2026-09-05 14:56:04 -0300

    add caminho para o sqlite e pagina de insert de produtos

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -6,4 +6,20 @@ public partial class ListaProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void ToolbarItem_Clicked(object sender, EventArgs e)
+	{
+
+		try
+		{
+			Navigation.PushAsync(new Views.NovoProduto());
+
+		}
+		catch (Exception ex)
+
+		{
+			DisplayAlert("Ops", ex.Message, "Ok");
+
+		}
+	}
 }
--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,9 +1,33 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
 {
-	public NovoProduto()
+    public NovoProduto()
 	{
 		InitializeComponent();
 	}
-}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso!", "Registro Inserido", "Ok");	
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "ok");
+		}
+    }
+
+   
+
+    }
